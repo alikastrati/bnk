9fbaee37b70578ec1e63c37de3c11f915ba72359
alikastrati <alikastrati5@gmail.com>
2024-09-02 23:46:55 +0200

    routing test

--- bnk/src/app/page.js
+++ bnk/src/app/page.js
@@ -1,7 +1,23 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+// import {
+//   NavigationMenu,
+//   NavigationMenuContent,
+//   NavigationMenuIndicator,
+//   NavigationMenuItem,
+//   NavigationMenuLink,
+//   NavigationMenuList,
+//   NavigationMenuTrigger,
+//   NavigationMenuViewport,
+// } from "@/components/ui/navigation-menu"
+
 
 export default function Home() {
   return (
-    <h1>Landing Page (Home)</h1>
-  );
+    <div>
+      <h1 className="text-amber-400">Landing Page</h1>
+      <Button>Click me</Button>
+
+
+    </div>
+  )
 }
